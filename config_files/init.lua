0ac06fb1e0114e02399c9b764fe711c326c8949d
fanny6213-web <phanishwar6123@gmail.com>
2024-12-26 13:42:51 +0530

    bug fix
    some  improvements

--- config_files/init.lua
+++ config_files/init.lua
@@ -1,1 +1,8 @@
 require("plugins")
+
+vim.api.nvim_set_hl(0, "CmpItemAbbr", { fg = "#ebdbb2", bg = "NONE" })
+vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#928374", bg = "NONE", strikethrough = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#fabd2f", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#fabd2f", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemKind", { fg = "#8ec07c", bg = "NONE" })
+vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#d3869b", bg = "NONE" })
